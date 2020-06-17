333d35674f53c3d86e9eb8339b82bd46dbb47f0b
Arvin Alvea <arvinalvea@gmail.com>
2020-06-17 13:23:43 -0400

    Added 'phrases' array

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const resetButton = document.querySelector('.btn__reset');
 let missed = 0; 
+let phrases = [
+    'A DIME A DOZEN',
+    'BREAK A LEG',
+    'SO FAR SO GOOD',
+    'UNDER THE WEATHER',
+    'BREAK THE ICE'
+]
